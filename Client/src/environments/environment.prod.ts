aa81ebe2a1b97f8ee0241c9471873eb79f8ce527
Oza29 <majo2@DESKTOP-4FTGB3H>
2026-07-12 15:08:24 +0300

    test frontend cicd #6

---
+++ Client/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'ecommerce-ALB-1489280742.us-east-1.elb.amazonaws.com/',
+   stripePublicKey:'pk_test_51RmYIaLtrZNVXaxLDzgu57A1nG4XbaLRhrCpZfJquCeDl1Di3PNYbmQSTz6N4aQ8Wj7gshfWMQ87cSpfvR2fb1G000MJD507h8'
+};
